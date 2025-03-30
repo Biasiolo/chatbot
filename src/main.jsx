639deac63a80d751ed278171d2e591e23439fc1f
Biasiolo <biasiolorafael@gmail.com>
2025-03-30 00:55:11 -0300

    manifest-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('✅ Service Worker registrado:', reg.scope))
-      .catch(err => console.log('❌ Erro ao registrar Service Worker:', err));
-  });
-}
+
 
